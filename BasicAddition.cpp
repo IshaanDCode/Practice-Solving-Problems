ea20f8313bf1c89371b9bc723764fd7b75b1671d
IshaanDCode <151793655+IshaanDCode@users.noreply.github.com>
2024-05-15 19:09:12 -0400

    Made name changes

---
+++ BasicAddition.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, j, k;
+    cin >> n >> j >> k;
+    
+    int addition;
+    addition =  n + j + k;
+    cout << addition << endl;
+    
+    return 0;
+}
