7f4ce5acd3d163aa236e8e3a030a8fe2abde25c8
Ishaan <151793655+IshaanDCode@users.noreply.github.com>
2024-05-17 08:56:14 -0400

    Add files via upload

---
+++ j12024.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int r; // red
+  int g; // green
+  int b; // blue
+  cin >> r;
+  cin >> g;
+  cin >> b;
+  int total;
+  total = r * 3 + g * 4 + b * 5;
+    cout << total;
+}
