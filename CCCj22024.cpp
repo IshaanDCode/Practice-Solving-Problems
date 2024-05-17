293a3240d98da6b056bf645adaf66616fc02a8dc
Ishaan <151793655+IshaanDCode@users.noreply.github.com>
2024-05-17 01:34:56 +0000

    Added my old code to it

--- CCCj22024.cpp
+++ CCCj22024.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int D;
+    cin >> D;
+    int size = D;
+
+    while (true) {
+        int Yobi;
+        cin >> Yobi;
+
+        if (Yobi < size) {
+            size += Yobi;
+        } else {
+            break;
+        }
+    }
+
+    cout << size << endl;
+
+    return 0;
+}
