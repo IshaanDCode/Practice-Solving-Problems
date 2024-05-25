1259ebf4ccf3c67852abf454f9570af1230a405f
IshaanDCode <151793655+IshaanDCode@users.noreply.github.com>
2024-05-25 12:23:11 -0400

    Made a new file

---
+++ RemoveElement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n;
+
+        for(int i = 0; i < n; i++){
+            cin >> n;
+            nums.push_back(n);
+            cout << nums[i];
+        }
+    }
+};
