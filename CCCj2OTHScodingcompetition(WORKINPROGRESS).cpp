f52aa92014396365f8b404d02c30f522952cde67
Ishaan <151793655+IshaanDCode@users.noreply.github.com>
2024-05-27 12:50:52 -0400

    Add files via upload

---
+++ CCCj2OTHScodingcompetition(WORKINPROGRESS).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;//number of explosions
+    vector<int> v;
+  for(int i = 0; i < n; i++){
+    int a;
+    cin >> a;
+    v.push_back(a);
+  }
+}
