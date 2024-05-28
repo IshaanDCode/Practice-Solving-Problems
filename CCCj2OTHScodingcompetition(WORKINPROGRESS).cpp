b610a0df165186bb116bac55d9659c0b94566730
Ishaan <151793655+IshaanDCode@users.noreply.github.com>
2024-05-28 12:24:51 -0400

    Update CCCj2OTHScodingcompetition(WORKINPROGRESS).cpp

--- CCCj2OTHScodingcompetition(WORKINPROGRESS).cpp
+++ CCCj2OTHScodingcompetition(WORKINPROGRESS).cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <numeric> // For accumulate
 using namespace std;
 
 int main(){
     int n;
-    cin >> n;//number of explosions
-    vector<int> v;
-  for(int i = 0; i < n; i++){
-    int a;
-    cin >> a;
-    v.push_back(a);
-  }
+    cin >> n; // number of explosions
+
+    vector<float> v;
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    int sum = accumulate(v.begin(), v.end(), 0.0);
+    cout << sum / v.size();
+
+    return 0;
 }
