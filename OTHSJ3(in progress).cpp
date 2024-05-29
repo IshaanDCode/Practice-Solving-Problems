b5162c71ddce4cf0fcbf14e52b831e89497ada09
Ishaan <151793655+IshaanDCode@users.noreply.github.com>
2024-05-29 12:26:22 -0400

    Update and rename OTHSJ3.cpp to OTHSJ3(in progress).cpp

---
+++ OTHSJ3(in progress).cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n; // number of bombs
+  int d; // the maximun for the hieght difference of the bomb 
+  
+}
