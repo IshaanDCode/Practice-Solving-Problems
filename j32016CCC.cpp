ee7de662a5b4ddbfe9cf56a46e9e4e9c8a16f276
Ishaan <151793655+IshaanDCode@users.noreply.github.com>
2024-05-29 12:50:19 -0400

    Update j32016CCC.cpp

--- j32016CCC.cpp
+++ j32016CCC.cpp
@@ -1,31 +1,22 @@
-// C++ program for the above approach
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-// Function to check whether string
-// is palindrome
-string isPalindrome(string S)
-{
-	// Iterate over the range [0, N/2]
-	for (int i = 0; i < S.length() / 2; i++) {
-
-		// If S[i] is not equal to
-		// the S[N-i-1]
-		if (S[i] != S[S.length() - i - 1]) {
-			// Return No
-			return "No";
-		}
-	}
-	// Return "Yes"
-	return "Yes";
+// Function to check whether string is palindrome
+string isPalindrome(const string &S) {
+    int n = S.length();
+    for (int i = 0; i < n / 2; ++i) {
+        if (S[i] != S[n - i - 1]) {
+            return "No";
+        }
+    }
+    return "Yes";
 }
 
 // Driver Code
-int main()
-{
-	string S = "ABCDCBA";
-	cout << isPalindrome(S);
-
-	return 0;
+int main() {
+    string S = "ABCDCBA";
+    cout << isPalindrome(S) << endl;
+    return 0;
 }
-
