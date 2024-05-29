44e6250431202de45b36e39196d167db9ffa2e79
Ishaan <151793655+IshaanDCode@users.noreply.github.com>
2024-05-29 12:32:20 -0400

    Update J12022Cupcakeparty.cpp

--- J12022Cupcakeparty.cpp
+++ J12022Cupcakeparty.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespaced std;
 
+int main(){
+  int b; //big boxes
+  int s; //small boxes
+  cin >> b;
+  cin >> s;
+  int total;
+}
