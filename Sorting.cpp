d7a0c8a221120f62c980e9f125a9c344c1ed65c7
IshaanDCode <151793655+IshaanDCode@users.noreply.github.com>
2024-05-30 09:27:50 -0400

    Made user input vector

--- Sorting.cpp
+++ Sorting.cpp
@@ -3,5 +3,14 @@
 #include <algorithm>
 using namespace std;
 int main(){
+    int n;
+    cin >> n;
+    vector<int> x;
+    int x;
+    for(int i = 0; i < n; i++){
+        cin x;
+        x.push_back(x);
+    }
     
 }
+ 
