58b3416b781c280acd135271dcf844a1feda9075
Ishaan <151793655+IshaanDCode@users.noreply.github.com>
2024-05-31 12:10:20 -0400

    Update An Animal Contest 2 P0 - Koala Matchmaking.cpp

--- An Animal Contest 2 P0 - Koala Matchmaking.cpp
+++ An Animal Contest 2 P0 - Koala Matchmaking.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iomanip>
-using namespace std;
-int main(){
-  double x;
-  cin >> x;
-  double total = 0;
-  total = x + (x - 2);
-  double z;
-  z = total / 2;
-  cout << z;  
-  cout << setprecision(2);
-  
-}
+#include <iomanip>  // Include iomanip for setprecision and fixed
 
+using namespace std;
+
+int main(){
+    double x;
+    cin >> x;
+    double total = 0;
+    total = x + (x - 2);
+    double z;
+    z = total / 2;
+    
+    // Set the precision and format output
+    cout << fixed << setprecision(2);
+    cout << z << endl;
+    
+    return 0;
+}
