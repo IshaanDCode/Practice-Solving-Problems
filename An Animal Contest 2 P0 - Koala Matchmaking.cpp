6778a9883940b720b8f72b0652acafba5ec60597
Ishaan <151793655+IshaanDCode@users.noreply.github.com>
2024-05-31 12:05:20 -0400

    Update An Animal Contest 2 P0 - Koala Matchmaking.cpp

--- An Animal Contest 2 P0 - Koala Matchmaking.cpp
+++ An Animal Contest 2 P0 - Koala Matchmaking.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+using namespace std;
 int main(){
   int x;
   cin >> x;
+  int total = 0;
+  total = x + (x - 2);
+  int z;
+  z = total / 2;
+  cout << z;
   
 }
 
