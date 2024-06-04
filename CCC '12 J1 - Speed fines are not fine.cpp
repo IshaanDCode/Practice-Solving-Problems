3e4cb26673e8708c5c7ccc261cfcfdd5136248e3
Ishaan Dhiman <151793655+IshaanDCode@users.noreply.github.com>
2024-06-04 10:30:01 -0400

    Update CCC '12 J1 - Speed fines are not fine.cpp

--- CCC '12 J1 - Speed fines are not fine.cpp
+++ CCC '12 J1 - Speed fines are not fine.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main(){
+  int x;
+  int y;
+  cin  >> x;
+  cin >> y;
+  int z;
+  z = y;
+  if (x >= y){
+    cout << "Congratulations, you are within the speed limit!";
+  }
+  else if(x < y + 20){
+    cout << "You are speeding and your fine is $500.";
+  }
+}
