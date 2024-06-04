ae35d89b1ef58858db7a6d299a7b7b05ac725615
Ishaan Dhiman <151793655+IshaanDCode@users.noreply.github.com>
2024-06-04 12:14:49 -0400

    Update CCC '12 J1 - Speed fines are not fine.cpp

--- CCC '12 J1 - Speed fines are not fine.cpp
+++ CCC '12 J1 - Speed fines are not fine.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-int main(){
-  int x;
-  int y;
-  cin  >> x;
-  cin >> y;
-  int z = y + 20;
-  int r = z + 30;
-  int q = r + 31;
-  if (y >= x){
-    cout << "Congratulations, you are within the speed limit!";
-}
-  else if(x < z){
-    cout << "You are speeding and your fine is $100.";
-  }
-  else if(x < r){
-    cout << "You are speedindg and your fine is $270.";
-  }
-  else if(x < q){
-    cout << "You are speeding and your fine is $500.";
-  }
+int main() {
+    int speed_limit, recorded_speed;
+
+    cin >> speed_limit >> recorded_speed;
+
+    if (recorded_speed <= speed_limit) {
+        cout << "Congratulations, you are within the speed limit!" << endl;
+    } else {
+        if (recorded_speed - speed_limit <= 20) {
+            cout << "You are speeding and your fine is $100." << endl;
+        } else if (recorded_speed - speed_limit <= 30) {
+            cout << "You are speeding and your fine is $270." << endl;
+        } else {
+            cout << "You are speeding and your fine is $500." << endl;
+        }
+    }
+
+    return 0;
 }
