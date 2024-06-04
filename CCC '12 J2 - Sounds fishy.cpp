99d234f49d6230e626e02e3419288ca10819c859
Ishaan Dhiman <151793655+IshaanDCode@users.noreply.github.com>
2024-06-04 13:29:06 -0400

    Update CCC '12 J2 - Sounds fishy.cpp

--- CCC '12 J2 - Sounds fishy.cpp
+++ CCC '12 J2 - Sounds fishy.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-int main(){
-  int x, y, z, a;
-  cin >> x;
-  cin >> y;
-  cin >> z;
-  cin >> a;
-if( x == y){
-  cout << "Fish At Constant Depth";
-}
-else if(x < y|| y < z|| z < x|| x < a){
-  cout << "Fish Rising";
-}
-  
-else if(x > y|| y > z|| z > x|| x > a){
-  cout << "Fish diving";
-}
-  else{ 
-  cout << "No Fish";
-  }
+int main() {
+    int x, y, z, a;
+    cin >> x >> y >> z >> a;
+    
+    if (x == y && y == z && z == a) {
+        cout << "Fish At Constant Depth";
+    } else if (x < y && y < z && z < a) {
+        cout << "Fish Rising";
+    } else if (x > y && y > z && z > a) {
+        cout << "Fish Diving";
+    } else {
+        cout << "No Fish";
+    }
+
+    return 0;
 }
