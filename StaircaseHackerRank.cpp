ccf33f9373e9a0949712a67780fc2ca27505de90
Ishaan Dhiman <151793655+IshaanDCode@users.noreply.github.com>
2024-06-12 12:40:17 -0400

    Update StaircaseHackerRank.cpp

--- StaircaseHackerRank.cpp
+++ StaircaseHackerRank.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main(){
+  int szam;
+cin >> szam;
+for (int i = 0; i < szam; i++) {
+
+    // This loop is new
+    for (int j = 0; j < szam-1+1; j++) {
+        cout << ' ';
+    }
+
+ 
+}
