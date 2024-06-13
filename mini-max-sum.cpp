c853844f0e1c8a570f5977f02c4bd6eec7c974a9
Ishaan Dhiman <151793655+IshaanDCode@users.noreply.github.com>
2024-06-12 23:08:47 -0400

    Update mini-max-sum.cpp

--- mini-max-sum.cpp
+++ mini-max-sum.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 using namespace std;
 
+
 int main() {
     vector<int> arr[5]; 
     for (int i = 0; i < 5; i++) { 
@@ -13,5 +14,6 @@ int main() {
     for(int j = 0; j < 5; j++){
          cout << arr[j][0] << " ";
     }
+    
     return 0; 
 }
